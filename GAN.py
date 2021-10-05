e107d97416bc3894b7854e06484fe95c82fc3fd0
JigSaW <misho_1998@mail.ru>
2021-10-05 22:32:34 +0300

    ne ponytno

--- GAN.py
+++ GAN.py
@@ -1,3 +1,4 @@
+
 from keras.layers import Input, Conv2D, Flatten, Dense, Conv2DTranspose, Reshape, Lambda, Activation, BatchNormalization, LeakyReLU, Dropout, ZeroPadding2D, UpSampling2D
 # from keras.layers.merge import _Merge
 
@@ -13,150 +14,329 @@ import os
 import pickle as pkl
 import matplotlib.pyplot as plt
 
+
 class GAN():
-    def __init__(self):
-        self.img_rows = 28
-        self.img_cols = 28
-        self.channels = 1
-        self.img_shape = (self.img_rows, self.img_cols, self.channels)
+    def __init__(self
+        , input_dim
+        , discriminator_conv_filters
+        , discriminator_conv_kernel_size
+        , discriminator_conv_strides
+        , discriminator_batch_norm_momentum
+        , discriminator_activation
+        , discriminator_dropout_rate
+        , discriminator_learning_rate
+        , generator_initial_dense_layer_size
+        , generator_upsample
+        , generator_conv_filters
+        , generator_conv_kernel_size
+        , generator_conv_strides
+        , generator_batch_norm_momentum
+        , generator_activation
+        , generator_dropout_rate
+        , generator_learning_rate
+        , optimiser
+        , z_dim
+        ):
 
-        optimizer = Adam(0.0002, 0.5)
+        self.name = 'gan'
 
-        # Build and compile the discriminator
-        self.discriminator = self.build_discriminator()
-        self.discriminator.compile(loss='binary_crossentropy',
-            optimizer=optimizer,
-            metrics=['accuracy'])
+        self.input_dim = input_dim
+        self.discriminator_conv_filters = discriminator_conv_filters
+        self.discriminator_conv_kernel_size = discriminator_conv_kernel_size
+        self.discriminator_conv_strides = discriminator_conv_strides
+        self.discriminator_batch_norm_momentum = discriminator_batch_norm_momentum
+        self.discriminator_activation = discriminator_activation
+        self.discriminator_dropout_rate = discriminator_dropout_rate
+        self.discriminator_learning_rate = discriminator_learning_rate
 
-        # Build and compile the generator
-        self.generator = self.build_generator()
-        self.generator.compile(loss='binary_crossentropy', optimizer=optimizer)
+        self.generator_initial_dense_layer_size = generator_initial_dense_layer_size
+        self.generator_upsample = generator_upsample
+        self.generator_conv_filters = generator_conv_filters
+        self.generator_conv_kernel_size = generator_conv_kernel_size
+        self.generator_conv_strides = generator_conv_strides
+        self.generator_batch_norm_momentum = generator_batch_norm_momentum
+        self.generator_activation = generator_activation
+        self.generator_dropout_rate = generator_dropout_rate
+        self.generator_learning_rate = generator_learning_rate
+        
+        self.optimiser = optimiser
+        self.z_dim = z_dim
 
-        # The generator takes noise as input and generated imgs
-        z = Input(shape=(100,))
-        img = self.generator(z)
+        self.n_layers_discriminator = len(discriminator_conv_filters)
+        self.n_layers_generator = len(generator_conv_filters)
 
-        # For the combined model we will only train the generator
-        self.discriminator.trainable = False
+        self.weight_init = RandomNormal(mean=0., stddev=0.02)
 
-        # The valid takes generated images as input and determines validity
-        valid = self.discriminator(img)
+        self.d_losses = []
+        self.g_losses = []
 
-        # The combined model  (stacked generator and discriminator) takes
-        # noise as input => generates images => determines validity
-        self.combined = Model(z, valid)
-        self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+        self.epoch = 0
 
-    def build_generator(self):
+        self._build_discriminator()
+        self._build_generator()
 
-        noise_shape = (100,)
+        self._build_adversarial()
 
-        model = Sequential()
+    def get_activation(self, activation):
+        if activation == 'leaky_relu':
+            layer = LeakyReLU(alpha = 0.2)
+        else:
+            layer = Activation(activation)
+        return layer
 
-        model.add(Dense(256, input_shape=noise_shape))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(Dense(512))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(Dense(1024))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(Dense(np.prod(self.img_shape), activation='tanh'))
-        model.add(Reshape(self.img_shape))
+    def _build_discriminator(self):
 
-        model.summary()
+        ### THE discriminator
+        discriminator_input = Input(shape=self.input_dim, name='discriminator_input')
 
-        noise = Input(shape=noise_shape)
-        img = model(noise)
+        x = discriminator_input
 
-        return Model(noise, img)
+        for i in range(self.n_layers_discriminator):
 
-    def build_discriminator(self):
+            x = Conv2D(
+                filters = self.discriminator_conv_filters[i]
+                , kernel_size = self.discriminator_conv_kernel_size[i]
+                , strides = self.discriminator_conv_strides[i]
+                , padding = 'same'
+                , name = 'discriminator_conv_' + str(i)
+                , kernel_initializer = self.weight_init
+                )(x)
 
-        img_shape = (self.img_rows, self.img_cols, self.channels)
+            if self.discriminator_batch_norm_momentum and i > 0:
+                x = BatchNormalization(momentum = self.discriminator_batch_norm_momentum)(x)
 
-        model = Sequential()
+            x = self.get_activation(self.discriminator_activation)(x)
 
-        model.add(Flatten(input_shape=img_shape))
-        model.add(Dense(512))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dense(256))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dense(1, activation='sigmoid'))
-        model.summary()
+            if self.discriminator_dropout_rate:
+                x = Dropout(rate = self.discriminator_dropout_rate)(x)
 
-        img = Input(shape=img_shape)
-        validity = model(img)
+        x = Flatten()(x)
+        
+        discriminator_output = Dense(1, activation='sigmoid', kernel_initializer = self.weight_init)(x)
 
-        return Model(img, validity)
-
-    def train(self, epochs, batch_size=128, save_interval=50):
-
-        # Load the dataset
-        (X_train, _), (_, _) = mnist.load_data()
-
-        # Rescale -1 to 1
-        X_train = (X_train.astype(np.float32) - 127.5) / 127.5
-        X_train = np.expand_dims(X_train, axis=3)
-
-        half_batch = int(batch_size / 2)
-
-        for epoch in range(epochs):
-
-            # ---------------------
-            #  Train Discriminator
-            # ---------------------
-
-            # Select a random half batch of images
-            idx = np.random.randint(0, X_train.shape[0], half_batch)
-            imgs = X_train[idx]
-
-            noise = np.random.normal(0, 1, (half_batch, 100))
-
-            # Generate a half batch of new images
-            gen_imgs = self.generator.predict(noise)
-
-            # Train the discriminator
-            d_loss_real = self.discriminator.train_on_batch(imgs, np.ones((half_batch, 1)))
-            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, np.zeros((half_batch, 1)))
-            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+        self.discriminator = Model(discriminator_input, discriminator_output)
 
 
-            # ---------------------
-            #  Train Generator
-            # ---------------------
+    def _build_generator(self):
 
-            noise = np.random.normal(0, 1, (batch_size, 100))
+        ### THE generator
 
-            # The generator wants the discriminator to label the generated samples
-            # as valid (ones)
-            valid_y = np.array([1] * batch_size)
+        generator_input = Input(shape=(self.z_dim,), name='generator_input')
 
-            # Train the generator
-            g_loss = self.combined.train_on_batch(noise, valid_y)
+        x = generator_input
 
-            # Plot the progress
-            print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
+        x = Dense(np.prod(self.generator_initial_dense_layer_size), kernel_initializer = self.weight_init)(x)
 
-            # If at save interval => save generated image samples
-            if epoch % save_interval == 0:
-                self.save_imgs(epoch)
+        if self.generator_batch_norm_momentum:
+            x = BatchNormalization(momentum = self.generator_batch_norm_momentum)(x)
 
-    def save_imgs(self, epoch):
-        r, c = 5, 5
-        noise = np.random.normal(0, 1, (r * c, 100))
+        x = self.get_activation(self.generator_activation)(x)
+
+        x = Reshape(self.generator_initial_dense_layer_size)(x)
+
+        if self.generator_dropout_rate:
+            x = Dropout(rate = self.generator_dropout_rate)(x)
+
+        for i in range(self.n_layers_generator):
+
+            if self.generator_upsample[i] == 2:
+                x = UpSampling2D()(x)
+                x = Conv2D(
+                    filters = self.generator_conv_filters[i]
+                    , kernel_size = self.generator_conv_kernel_size[i]
+                    , padding = 'same'
+                    , name = 'generator_conv_' + str(i)
+                    , kernel_initializer = self.weight_init
+                )(x)
+            else:
+
+                x = Conv2DTranspose(
+                    filters = self.generator_conv_filters[i]
+                    , kernel_size = self.generator_conv_kernel_size[i]
+                    , padding = 'same'
+                    , strides = self.generator_conv_strides[i]
+                    , name = 'generator_conv_' + str(i)
+                    , kernel_initializer = self.weight_init
+                    )(x)
+
+            if i < self.n_layers_generator - 1:
+
+                if self.generator_batch_norm_momentum:
+                    x = BatchNormalization(momentum = self.generator_batch_norm_momentum)(x)
+
+                x = self.get_activation(self.generator_activation)(x)
+                    
+                
+            else:
+
+                x = Activation('tanh')(x)
+
+
+        generator_output = x
+
+        self.generator = Model(generator_input, generator_output)
+
+       
+    def get_opti(self, lr):
+        if self.optimiser == 'adam':
+            opti = Adam(lr=lr, beta_1=0.5)
+        elif self.optimiser == 'rmsprop':
+            opti = RMSprop(lr=lr)
+        else:
+            opti = Adam(lr=lr)
+
+        return opti
+
+    def set_trainable(self, m, val):
+        m.trainable = val
+        for l in m.layers:
+            l.trainable = val
+
+
+    def _build_adversarial(self):
+        
+        ### COMPILE DISCRIMINATOR
+
+        self.discriminator.compile(
+        optimizer=self.get_opti(self.discriminator_learning_rate)  
+        , loss = 'binary_crossentropy'
+        ,  metrics = ['accuracy']
+        )
+        
+        ### COMPILE THE FULL GAN
+
+        self.set_trainable(self.discriminator, False)
+
+        model_input = Input(shape=(self.z_dim,), name='model_input')
+        model_output = self.discriminator(self.generator(model_input))
+        self.model = Model(model_input, model_output)
+
+        self.model.compile(optimizer=self.get_opti(self.generator_learning_rate) , loss='binary_crossentropy', metrics=['accuracy'])
+
+        self.set_trainable(self.discriminator, True)
+
+
+
+    
+    def train_discriminator(self, x_train, batch_size, using_generator):
+
+        valid = np.ones((batch_size,1))
+        fake = np.zeros((batch_size,1))
+
+        if using_generator:
+            true_imgs = next(x_train)[0]
+            if true_imgs.shape[0] != batch_size:
+                true_imgs = next(x_train)[0]
+        else:
+            idx = np.random.randint(0, x_train.shape[0], batch_size)
+            true_imgs = x_train[idx]
+        
+        noise = np.random.normal(0, 1, (batch_size, self.z_dim))
         gen_imgs = self.generator.predict(noise)
 
-        # Rescale images 0 - 1
-        gen_imgs = 0.5 * gen_imgs + 0.5
+        d_loss_real, d_acc_real =   self.discriminator.train_on_batch(true_imgs, valid)
+        d_loss_fake, d_acc_fake =   self.discriminator.train_on_batch(gen_imgs, fake)
+        d_loss =  0.5 * (d_loss_real + d_loss_fake)
+        d_acc = 0.5 * (d_acc_real + d_acc_fake)
 
-        fig, axs = plt.subplots(r, c)
+        return [d_loss, d_loss_real, d_loss_fake, d_acc, d_acc_real, d_acc_fake]
+
+    def train_generator(self, batch_size):
+        valid = np.ones((batch_size,1))
+        noise = np.random.normal(0, 1, (batch_size, self.z_dim))
+        return self.model.train_on_batch(noise, valid)
+
+
+    def train(self, x_train, batch_size, epochs, run_folder
+    , print_every_n_batches = 50
+    , using_generator = False):
+
+        for epoch in range(self.epoch, self.epoch + epochs):
+
+            d = self.train_discriminator(x_train, batch_size, using_generator)
+            g = self.train_generator(batch_size)
+
+            print ("%d [D loss: (%.3f)(R %.3f, F %.3f)] [D acc: (%.3f)(%.3f, %.3f)] [G loss: %.3f] [G acc: %.3f]" % (epoch, d[0], d[1], d[2], d[3], d[4], d[5], g[0], g[1]))
+
+            self.d_losses.append(d)
+            self.g_losses.append(g)
+
+            if epoch % print_every_n_batches == 0:
+                self.sample_images(run_folder)
+                self.model.save_weights(os.path.join(run_folder, 'weights/weights-%d.h5' % (epoch)))
+                self.model.save_weights(os.path.join(run_folder, 'weights/weights.h5'))
+                self.save_model(run_folder)
+
+            self.epoch += 1
+
+    
+    def sample_images(self, run_folder):
+        r, c = 5, 5
+        noise = np.random.normal(0, 1, (r * c, self.z_dim))
+        gen_imgs = self.generator.predict(noise)
+
+        gen_imgs = 0.5 * (gen_imgs + 1)
+        gen_imgs = np.clip(gen_imgs, 0, 1)
+
+        fig, axs = plt.subplots(r, c, figsize=(15,15))
         cnt = 0
+
         for i in range(r):
             for j in range(c):
-                axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
+                axs[i,j].imshow(np.squeeze(gen_imgs[cnt, :,:,:]), cmap = 'gray')
                 axs[i,j].axis('off')
                 cnt += 1
-        fig.savefig("gan/images/mnist_%d.png" % epoch)
+        fig.savefig(os.path.join(run_folder, "images/sample_%d.png" % self.epoch))
         plt.close()
+
+
+
+
+    
+    def plot_model(self, run_folder):
+        plot_model(self.model, to_file=os.path.join(run_folder ,'viz/model.png'), show_shapes = True, show_layer_names = True)
+        plot_model(self.discriminator, to_file=os.path.join(run_folder ,'viz/discriminator.png'), show_shapes = True, show_layer_names = True)
+        plot_model(self.generator, to_file=os.path.join(run_folder ,'viz/generator.png'), show_shapes = True, show_layer_names = True)
+
+
+
+    def save(self, folder):
+
+        with open(os.path.join(folder, 'params.pkl'), 'wb') as f:
+            pkl.dump([
+                self.input_dim
+                , self.discriminator_conv_filters
+                , self.discriminator_conv_kernel_size
+                , self.discriminator_conv_strides
+                , self.discriminator_batch_norm_momentum
+                , self.discriminator_activation
+                , self.discriminator_dropout_rate
+                , self.discriminator_learning_rate
+                , self.generator_initial_dense_layer_size
+                , self.generator_upsample
+                , self.generator_conv_filters
+                , self.generator_conv_kernel_size
+                , self.generator_conv_strides
+                , self.generator_batch_norm_momentum
+                , self.generator_activation
+                , self.generator_dropout_rate
+                , self.generator_learning_rate
+                , self.optimiser
+                , self.z_dim
+                ], f)
+
+        self.plot_model(folder)
+
+    def save_model(self, run_folder):
+        self.model.save(os.path.join(run_folder, 'model.h5'))
+        self.discriminator.save(os.path.join(run_folder, 'discriminator.h5'))
+        self.generator.save(os.path.join(run_folder, 'generator.h5'))
+        pkl.dump(self, open( os.path.join(run_folder, "obj.pkl"), "wb" ))
+
+    def load_weights(self, filepath):
+        self.model.load_weights(filepath)
+
+
+        
+
+
